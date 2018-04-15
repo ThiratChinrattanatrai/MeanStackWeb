7d8b30b89a18c926d4829c38d8971f5c8f2f5b69
Thirat <whalefrog@hotmail.com>
2018-04-16 01:38:13 +0700

    fix bug

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require('passport');
 let app = express();
 
 // port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // connect to database
 mongoose.connect(config.database);
